728a09c61dfa9dc979f7a36e5ac442b39ba23729
Alexandr <scad.luncher@gmail.com>
2023-05-11 19:05:01 +0300

    basic filtering - week and group

--- mod/index.js
+++ mod/index.js
@@ -1,16 +1,78 @@
 $(document).ready(function () {
-    $.ajax({
-        type: "get",
-        url: "./process.php",
-        data: {},
-        success: function (response) {
-            let resP = JSON.parse(response);
-            resP.forEach(element => {
-                if(element != ""){
-                    $("#weekTT").html( $("#weekTT").html()+'<li><a class="dropdown-item" href="#">'+element+'</a></li>' );
+
+    function ajReqData(sendData, respType) {
+
+        $.ajax({
+            type: "get",
+            url: "./process.php",
+            data: sendData,
+            success: function (response) {
+
+                let resP = JSON.parse(response);
+                console.log(resP);
+
+                switch (respType) {
+                    case 1:
+                        fillDrop("#weekTT", resP.pages, "wk");
+                        $(".wk").removeClass("active");
+                        $(".wk").click(function (e) { 
+                            e.preventDefault();
+                            ajReqData(
+                                {'weekN':$(this).html()}, 
+                                2
+                            );
+                            $(this).addClass("active");
+                        });
+                        break;
+
+                        case 2:
+
+                            fillDrop("#groupTT", resP.groups, "gp");
+                            fillDrop("#wekDayTT", resP.weekDays, "wd");
+
+                            $("#groupTTDrop").removeClass("disabled");
+                            $(".gp").removeClass("active");
+
+                            $("#wekDayTTDrop").removeClass("disabled");
+                            $(".wd").removeClass("active");
+
+                            $(".wd").click(function (e) { 
+                                e.preventDefault();
+                                ajReqData(
+                                    {
+                                        'gpN': $(".gp.active").html(),
+                                        'gpCol': $(".gp.active").attr("index"),
+                                        'weekN': $(".wk.active").html(),
+                                        'wkDay': $(this).attr("index")
+                                    }, 
+                                    3
+                                );
+                                $(this).addClass("active");
+                                //alert($(".wk.active").html())
+                            });
+                        break
+                    default:
+
+                        break;
                 }
-            });
-            console.log(resP);
-        }
-    });
+
+
+
+            }
+        });
+    }
+
+    function fillDrop(contID, dataArr, bindClass){
+        dataArr.forEach((element, ind) => {
+            if (element != "") {
+                $(contID).html(
+                    $(contID).html() + 
+                    '<li><a class="dropdown-item '+bindClass+'" index="'+ind+'" href="#">' + element + '</a></li>'
+                    );
+            }
+        });
+    }
+
+    ajReqData(null, 1);
+
 });
--- process.php
+++ process.php
@@ -11,10 +11,32 @@ $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xlsx");
 $reader->setReadDataOnly(TRUE);
 
 $ss = $reader->load("ionmo_22_o_m.xlsx");
-$resPonse[] = array();
+
+class resPonseStruct {
+    public $pages = array();
+    public $groups = array();
+    public $timetable = array();
+    public $weekDays = array("Понедельник","Вторник","Среда","Четверг","Пятница","Суббота");
+}
+$resPonse = new resPonseStruct;
+
+
+const GPROW = 4;
 
 foreach($ss->getWorksheetIterator() as $Sh){
-    $resPonse[] = $Sh->getTitle();
+    //$page->title = $Sh->getTitle();
+    $resPonse->pages[] = $Sh->getTitle();
+}
+
+if(isset($_GET['weekN'])){
+    $ws = $ss->getSheetByName($_GET['weekN']);
+    $topCol = $ws->getHighestColumn();
+    $resPonse->groups = $ws->rangeToArray("A".GPROW.":".$topCol.GPROW, "", false, false, false)[0];
+}
+
+if(isset($_GET['gpN']) && isset($_GET['gpCol']) && isset($_GET['weekN'])){
+    $ws = $ss->getSheetByName($_GET['weekN']);
+    $resPonse->timetable = $ws->rangeToArray("A1:A3", "", false, false, false)
 }
 
 print(json_encode($resPonse));
